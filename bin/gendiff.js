15bd5a7dfd22601bc879aef3475e0d49558a4b3c
Alexander Sergeev <aleksanteri.sergeev@gmail.com>
2020-11-14 19:11:04 +0200

    Modificated help output

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -6,5 +6,7 @@ const { program } = require('commander');
 
 program
   .version('1.0.0', '-V, --version', 'output the version number')
-  .description('Compares two configuration files and shows a difference.');
+  .description('Compares two configuration files and shows a difference.')
+  .arguments('<filepath1> <filepath2>')
+  .option('-f --format [type]', 'output format');
 program.parse(process.argv);
