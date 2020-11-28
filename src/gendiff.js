9d4d0a778d5ce303e3423aa06ee2db51d03376a7
Alexander Sergeev <aleksanteri.sergeev@gmail.com>
2020-11-28 19:29:47 +0200

    Changed import lodash

--- src/gendiff.js
+++ src/gendiff.js
@@ -1,18 +1,14 @@
-import lodash from 'lodash';
-
-const {
-  union, isObject, has, sortBy,
-} = lodash;
+import _ from 'lodash';
 
 const generateDiff = (data1, data2) => {
   const getDifference = (file1, file2, key) => {
-    if (!has(file1, key)) {
+    if (!_.has(file1, key)) {
       return { key, type: 'added', value: file2[key] };
     }
-    if (!has(file2, key)) {
+    if (!_.has(file2, key)) {
       return { key, type: 'removed', value: file1[key] };
     }
-    if (isObject(file1[key]) && isObject(file2[key])) {
+    if (_.isObject(file1[key]) && _.isObject(file2[key])) {
       return { key, type: 'nested', children: generateDiff(file1[key], file2[key]) };
     }
     if (file1[key] !== file2[key]) {
@@ -23,7 +19,7 @@ const generateDiff = (data1, data2) => {
     return { key, type: 'unchanged', value: file1[key] };
   };
 
-  const keys = sortBy(union(Object.keys(data1), Object.keys(data2)));
+  const keys = _.sortBy(_.union(Object.keys(data1), Object.keys(data2)));
   return keys.map((key) => getDifference(data1, data2, key));
 };
 
